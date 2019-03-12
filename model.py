c044b0b1e7962609eb139a0741a17549e2bc7c94
Annie <ansross@cs.washington.edu>
2019-03-12 09:33:36 -0700

    attempt to add mem

--- model.py
+++ model.py
@@ -35,10 +35,10 @@ class Model(torch.nn.Module):
         self.conv4 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
         self.maxp4 = nn.MaxPool2d(2, 2)
 
-        self.lstm = nn.LSTMCell(1024, args.hidden_state_sz)
+        self.lstm = nn.LSTMCell(1024+300, args.hidden_state_sz)
         self.critic_linear = nn.Linear(args.hidden_state_sz, 1)
         self.actor_linear = nn.Linear(args.hidden_state_sz, args.action_space)
-        self.augmented_linear = nn.Linear(2, 1024)
+        self.augmented_linear = nn.Linear(2, 300)
         self.augmented_combination = nn.Linear(1024 + 300, 1024)
 
 
